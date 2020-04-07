586e59d6c06354db4cffca64b69e73d4b60a8554
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-04-07 19:54:33 -0300

    create(1279.py): added 1279 uri answer - imcomplete

---
+++ 1279.py
@@ -0,0 +1,28 @@
+# URI - 1279
+
+def define_year(year: int):
+    festival = False
+
+    if (year % 4 == 0 and not year % 100 == 0) or (year % 400 == 0):
+        festival = True
+        yield 'This is leap year'
+    if year % 15 == 0:
+        festival = True
+        yield 'This is huluculu festival year.'
+    if year % 55 == 0:
+        festival = True
+        yield 'This is bulukulu festival year'
+    
+    if not festival:
+        yield 'This is an ordinary year.'
+
+years = list()
+while True:
+    try:
+        years.append(int(input()))
+    except EOFError:
+        break
+
+for y in years:
+    print(*define_year(y), sep='\n')
+    print()
